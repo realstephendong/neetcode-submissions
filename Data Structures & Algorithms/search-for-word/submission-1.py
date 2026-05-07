2ab096e4611b8f5e00d770dfa391a5d738e85362
Stephen Dong <realstephendong@gmail.com>
2026-05-07 08:36:00 -0400

    Add: search-for-word - submission-1

---
+++ Data Structures & Algorithms/search-for-word/submission-1.py
@@ -0,0 +1,47 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+    
+
+        # parent coordinates as parameter? check if visiting parent coordinates before making the backtrack
+
+        # run backtrack recursively on each vertically/horizontally adjacent path as long as it does not exceed
+        # the boundaries of the board
+
+        wordlen = len(word)
+        rows = len(board)
+        cols = len(board[0])
+
+        def backtrack(x, y, letterCount):
+
+            if board[x][y] != word[letterCount]:
+                return False
+
+            if letterCount == wordlen - 1:
+                return True
+
+            letterCount += 1
+            visited.add((x,y))
+
+            if x + 1 < rows and (x+1, y) not in visited:
+                if backtrack(x+1, y, letterCount):
+                    return True
+            if x - 1 >= 0 and (x-1, y) not in visited:
+                if backtrack(x-1, y, letterCount):
+                    return True
+            if y + 1 < cols and (x, y+1) not in visited:
+                if backtrack(x, y+1, letterCount):
+                    return True
+            if y - 1 >= 0 and (x, y-1) not in visited:
+                if backtrack(x, y-1, letterCount):
+                    return True
+            visited.remove((x,y))
+            return False
+                    
+    
+        for row in range(rows):
+            for col in range(cols):
+                visited = set()
+                if backtrack(row, col, 0):
+                    return True
+        return False
+            
